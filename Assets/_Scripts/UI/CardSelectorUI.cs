1d07a367b09dad7452c474f14409d267017847fc
hikidev96 <dpalstn91@gmail.com>
2022-07-18 20:32:46 +0900

    feature card-selection-sound

--- Assets/_Scripts/UI/CardSelectorUI.cs
+++ Assets/_Scripts/UI/CardSelectorUI.cs
@@ -4,14 +4,22 @@ namespace SOD.UI
 {
     public class CardSelectorUI : UIBehaviour
     {
+        [SerializeField] private AudioData openAudioData;
+
         protected virtual void OnEnable()
         {
             ServiceProvider.GameService.Pause();
+            PlayOpenSound();
         }
 
         protected virtual void OnDisable()
         {
             ServiceProvider.GameService.Unpause();
+        }
+
+        protected void PlayOpenSound()
+        {
+            ServiceProvider.AudioService.Play(openAudioData);
         }
     }
 }
--- Assets/_Scripts/UI/CardUI.cs
+++ Assets/_Scripts/UI/CardUI.cs
@@ -7,16 +7,21 @@ namespace SOD.UI
 {
     public class CardUI : UIBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
+        [SerializeField] private AudioData hoverAudioData;
+        [SerializeField] private AudioData selectAudioData;
+
         public UnityEvent OnSelected { get; private set; } = new UnityEvent();
 
         virtual public void OnPointerClick(PointerEventData eventData)
         {
             OnSelected.Invoke();
+            ServiceProvider.AudioService.Play(selectAudioData);
         }
 
         virtual public void OnPointerEnter(PointerEventData eventData)
         {
             this.transform.DOScale(1.1f, 0.2f).SetUpdate(true);
+            ServiceProvider.AudioService.Play(hoverAudioData);
         }
 
         virtual public void OnPointerExit(PointerEventData eventData)
